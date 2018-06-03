9dfc81d1168c8544df4a8ee64cfd3e88fabefacb
Anh-Phuc Ngo <anhphuc.ngo@gmail.com>
2018-06-03 05:13:29 -0400

    [emacs] Store emacs backups in one place

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -226,3 +226,11 @@
 (recentf-mode 1)
 (setq recentf-max-menu-items 100
       recentf-max-saved-items 100)
+
+(setq backup-by-copying t ; don't clobber symlinks
+      backup-directory-alist
+      '(("." . "~/.emacs.d/saves/")) ; don't litter my fs tree
+      delete-old-versions t
+      kept-new-versions 6
+      kept-old-versions 2
+      version-control t) ; use versioned backups
